13128fbcf1d3c496e3a5d271b9fb7bd311a749ef
chrisxxmas <119321198+chrisxxmas@users.noreply.github.com>
2023-02-22 21:51:02 +0000

    Improve++

---
+++ YamlDotNet.AotTest/run.sh
@@ -0,0 +1,6 @@
+
+#!/bin/sh
+
+mono --aot=full ./bin/Release/net45/YamlDotNet.dll && \
+    mono --aot=full ./bin/Release/net45/YamlDotNet.AotTest.exe && \
+    mono --full-aot ./bin/Release/net45/YamlDotNet.AotTest.exe
