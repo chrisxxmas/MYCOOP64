4e2daf4a4a1b11b8b21b993648c9a5e8e0bf7027
chrisxxmas <119321198+chrisxxmas@users.noreply.github.com>
2023-02-24 22:22:22 +0000

    v6.1.2

--- docker-build.sh
+++ docker-build.sh
@@ -4,3 +4,6 @@
 if [ -d ../YamlDotNet.wiki ]
   then
     docker run -u=`id -u` -v `pwd`:/build/YamlDotNet -v `pwd`/../YamlDotNet.wiki:/build/YamlDotNet.wiki -v ~/.nuget/packages:/home/build/.nuget/packages -w /build/YamlDotNet -it aaubry/yamldotnet.local ./build.sh "$@"
+  else
+    docker run -u=`id -u` -v `pwd`:/build/YamlDotNet -v ~/.nuget/packages:/home/build/.nuget/packages -w /build/YamlDotNet -it aaubry/yamldotnet.local ./build.sh "$@"
+fi
---
+++ tools/parsers/reference/run.sh
@@ -0,0 +1,8 @@
+
+
+if [ "$1" == "--html" ]; then
+    /app/dist/build/yaml2yeast/yaml2yeast <&0 | /app/yeast2html
+else
+    echo "Use --html for HTML output"
+    /app/dist/build/yaml2yeast/yaml2yeast <&0 | grep --color -E '^!.*|$'
+fi
