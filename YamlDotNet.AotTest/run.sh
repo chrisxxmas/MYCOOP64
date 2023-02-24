f76f17e84e9946210dc9ae45f7ad045ae94ec3ba
chrisxxmas <119321198+chrisxxmas@users.noreply.github.com>
2023-02-24 06:40:31 +0000

    travis fix?

--- YamlDotNet.AotTest/run.sh
+++ YamlDotNet.AotTest/run.sh
@@ -4,3 +4,5 @@
 mono --aot=full ./bin/Release/net45/YamlDotNet.dll && \
     mono --aot=full ./bin/Release/net45/YamlDotNet.AotTest.exe && \
     mono --full-aot ./bin/Release/net45/YamlDotNet.AotTest.exe
+
+echo $? > exitcode.txt
